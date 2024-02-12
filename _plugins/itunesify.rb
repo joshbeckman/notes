6afa2e0c22f87fdf9e90a055071092829ec90e3d
Josh Beckman <email@andjosh.com>
2024-02-11 21:28:30 -0600

    new(usr): Add liquid filter for itunes embeds

---
+++ _plugins/itunesify.rb
@@ -0,0 +1,17 @@
+require 'uri'
+
+module Jekyll
+  module ItunesifyFilter
+    def itunesify(input)
+      puts input
+      url = URI(input)
+      url.host = "embed.music.apple.com"
+      block = <<-HTML
+<iframe allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write" frameborder="0" height="450" style="width:100%;max-width:660px;overflow:hidden;border-radius:10px;" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="#{url.to_s}"></iframe>
+      HTML
+      block
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::ItunesifyFilter)
