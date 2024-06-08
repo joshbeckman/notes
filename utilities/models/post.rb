1781f8d55da156b999d755105fe9226e0f8e48e7
Josh Beckman <email@andjosh.com>
2024-06-08 11:38:53 -0500

    chg(dev): Obey the rubocops

--- utilities/models/post.rb
+++ utilities/models/post.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 
 require 'json'
 require 'net/http'
@@ -15,43 +16,38 @@ Post = Struct.new(
   :title,
   keyword_init: true
 ) do
-
   def movie_cover_image
-    key = ENV["OMDB_TOKEN"]
+    key = ENV['OMDB_TOKEN']
     unless key
-      puts "OMDB_TOKEN environment variable not set"
+      puts 'OMDB_TOKEN environment variable not set'
       return
     end
     url = "https://www.omdbapi.com/?i=#{imdb_id}&apikey=#{key}"
     uri = URI(url)
     response = Net::HTTP.get(uri)
     json = JSON.parse(response)
-    json["Poster"]
+    json['Poster']
   end
 
   def create_file
-    filename = "#{category}/_posts/#{date.strftime("%Y-%m-%d")}-#{slug}.md"
+    filename = "#{category}/_posts/#{date.strftime('%Y-%m-%d')}-#{slug}.md"
 
     File.open(filename, 'w') do |file|
-      file.puts "---"
-      file.puts "layout: Post"
-      file.puts "date: #{date.strftime("%Y-%m-%d %H:%M:%S %z")}"
+      file.puts '---'
+      file.puts 'layout: Post'
+      file.puts "date: #{date.strftime('%Y-%m-%d %H:%M:%S %z')}"
       file.puts "title: \"#{title}\""
-      file.puts "toc: true"
-      if rating
-        file.puts "rating: #{rating}"
-      end
-      if imdb_id
-        file.puts "imdb_id: #{imdb_id}"
-      end
+      file.puts 'toc: true'
+      file.puts "rating: #{rating}" if rating
+      file.puts "imdb_id: #{imdb_id}" if imdb_id
       file.puts "image: #{image}"
-      file.puts "description: "
-      file.puts "tags: "
+      file.puts 'description: '
+      file.puts 'tags: '
       tags.each do |tag|
         file.puts "  - #{tag}"
       end
-      file.puts "---"
-      file.puts ""
+      file.puts '---'
+      file.puts ''
       file.puts body
     end
     filename
