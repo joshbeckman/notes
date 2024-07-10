255a750e07df442c0457b6de3e9b55c515f34818
Josh Beckman <email@andjosh.com>
2024-07-09 21:25:15 -0500

    new(usr): Add support for user-defined topic/question on insight

--- assets/js/insight.js
+++ assets/js/insight.js
@@ -1,14 +1,55 @@
 (function(){
-    var url = 'https://joshbeckman-amethysthalibut.web.val.run';
-    var topic = new URLSearchParams(window.location.search).get('topic');
+    var topicInsightUrl = 'https://joshbeckman-amethysthalibut.web.val.run';
+    var questionInsightUrl = 'https://joshbeckman-coffeeostrich.web.val.run';
+    var params = new URLSearchParams(window.location.search);
+    var topic = params.get('topic');
+    var question = params.get('question');
+
     if (topic) {
-        url += '?topic=' + encodeURIComponent(topic);
+        hideMenu();
+        fetchTopicInsight(topic);
+    } else if (question) {
+        hideMenu();
+        fetchQuestionInsight(question);
     }
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('insight').innerHTML = data.insightHtml;
-            document.getElementById('topic').innerHTML = data.topic;
-        });
+
+    function startLoading() {
+        document.getElementById('insight-loading').style.display = 'block';
+    }
+    function stopLoading() {
+        document.getElementById('insight-loading').style.display = 'none';
+    }
+    function hideMenu() {
+        document.getElementById('insight-menu').style.display = 'none';
+    }
+    function fetchTopicInsight(topic) {
+        startLoading();
+        if (topic == 'random') {
+            topic = null;
+        }
+        if (topic) {
+            document.getElementById('topic').innerHTML = topic;
+            topicInsightUrl += '?topic=' + encodeURIComponent(topic);
+        }
+        fetch(topicInsightUrl)
+            .then(response => response.json())
+            .then(data => {
+                stopLoading();
+                document.getElementById('insight').innerHTML = data.insightHtml;
+                document.getElementById('topic').innerHTML = data.topic;
+            });
+    }
+    function fetchQuestionInsight(question) {
+        startLoading();
+        document.getElementById('topic').innerHTML = question;
+        questionInsightUrl += '?question=' + encodeURIComponent(question);
+        fetch(questionInsightUrl)
+            .then(response => response.json())
+            .then(data => {
+                stopLoading();
+                document.getElementById('insight').innerHTML = data.insightHtml;
+                document.getElementById('topic').innerHTML = data.question;
+            });
+    }
 
 })();
--- insight.md
+++ insight.md
@@ -14,6 +14,36 @@ This page generates a random "insight" from all my posts - you can [read about h
 
 Each time you come to this page, it will attempt to generate a new insight. Note that, to prevent abuse, this is currently limited to 10 insights/min (this is across all users: _please share/play nicely_).
 
+<div id="insight-menu">
+<details name="insight-options" open>
+<summary>Random Topic</summary>
+<p>
+<form id="insight-form-random" method="GET">
+  <input type="hidden" name="topic" value="random">
+  <button type="submit">Generate Insight</button>
+</form>
+</p>
+</details>
+<details name="insight-options">
+<summary>Specific Topic</summary>
+<p>
+<form id="insight-form-topic" method="GET">
+  <input type="text" name="topic" placeholder="Specify a topic...">
+  <button type="submit">Generate Insight</button>
+</form>
+</p>
+</details>
+<details name="insight-options">
+<summary>Ask a Question</summary>
+<p>
+<form id="insight-form-question" method="GET">
+  <input type="text" name="question" placeholder="Your question...">
+  <button type="submit">Ask</button>
+</form>
+</p>
+</details>
+</div>
 <h2 id="topic"></h2>
-<div id="insight"><em>Generating insight...</em></div>
+<p id="insight-loading" style="display:none"><em>Generating insight...</em></p>
+<div id="insight"></div>
 <script src="/assets/js/insight.js"></script>
