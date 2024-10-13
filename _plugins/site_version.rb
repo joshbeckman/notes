b65af3f31011b45a6fa84a56661f5e20f72757c1
Josh Beckman <email@andjosh.com>
2024-10-12 23:36:57 -0500

    new(usr): Define site version

---
+++ _plugins/site_version.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class SiteVersion
+  def initialize(site)
+    @site = site
+  end
+
+  def inject
+    @site.config['version'] = version
+  end
+
+  # Returns the version of the site
+  # If the site is a git repository, it returns the output of `git describe --long`
+  # Otherwise, it returns a string with the number of commits and the short hash of the last commit
+  # ref: https://news.ycombinator.com/item?id=28155654
+  def version
+    `git describe --long 2>/dev/null | sed 's/\([^-]*-g\)/r\1/' || \
+      printf "r%s-g%s" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)"`
+  end
+end
+Jekyll::Hooks.register :site, :after_init do |site|
+  SiteVersion.new(site).inject
+end
--- humans.md
+++ humans.md
@@ -7,3 +7,4 @@ searchable: true
 {{ site.data.proverbs | sample }}
 {% assign month = site.time | date: "%m" | plus: 0 %}{{ site.data.seasons[month].emoji }}
 Updated: {{ site.time | date_to_xmlschema }}
+Version: {{ site.version }}
