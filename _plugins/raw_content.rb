ec62ad79b95aeb68844e6c2aa973abbf6649b171
Josh Beckman <josh@joshbeckman.org>
2024-12-02 08:38:08 -0600

    fix(dev): Fix page access in generator

--- _plugins/raw_content.rb
+++ _plugins/raw_content.rb
@@ -19,7 +19,7 @@ module RawContent
         end.compact
         post.data['backlinks'] = (post_backlinks + page_backlinks).compact
       end
-      site.pages.docs.each do |page|
+      site.pages.each do |page|
         page.data['raw_content'] = page.content
         post_backlinks = site.posts.docs.map do |other_post|
           next unless other_post.content.include?(page.url)
