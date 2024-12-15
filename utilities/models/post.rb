7d315a8d74172c2e60197c137e9754fd830c70ec
Josh Beckman <email@andjosh.com>
2024-12-15 10:57:51 -0600

    new(dev): Support PESOS from Letterboxd

--- utilities/models/post.rb
+++ utilities/models/post.rb
@@ -12,10 +12,13 @@ Post = Struct.new(
   :image,
   :date,
   :imdb_id,
+  :tmdb_id,
+  :letterboxd_id,
   :in_reply_to,
   :hide_title,
   :mastodon_social_status_url,
   :bluesky_status_url,
+  :letterboxd_review_url,
   :rating,
   :slug,
   :tags,
@@ -48,11 +51,14 @@ Post = Struct.new(
       file.puts "canonical: #{canonical}" if canonical
       file.puts "rating: #{rating}" if rating
       file.puts "imdb_id: #{imdb_id}" if imdb_id
+      file.puts "tmdb_id: #{tmdb_id}" if tmdb_id
+      file.puts "letterboxd_id: #{letterboxd_id}" if letterboxd_id
       file.puts "image: #{image}"
       file.puts "description: #{description}"
       file.puts "in_reply_to: #{in_reply_to}" if in_reply_to
       file.puts "mastodon_social_status_url: #{mastodon_social_status_url}" unless mastodon_social_status_url.nil?
       file.puts "bluesky_status_url: #{bluesky_status_url}" unless bluesky_status_url.nil?
+      file.puts "letterboxd_review_url: #{letterboxd_review_url}" unless letterboxd_review_url.nil?
       if tags&.any?
         file.puts 'tags:'
         tags.each do |tag|
