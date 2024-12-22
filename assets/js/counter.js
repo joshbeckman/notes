44aade6967f54368b37ef432b06303eea674a101
Josh Beckman <email@andjosh.com>
2024-12-22 16:03:02 -0600

    chg(usr): Only increment the counter

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -3,7 +3,6 @@ let grow = document.getElementById('grow');
 let kill = document.getElementById('kill');
 let url = 'https://joshbeckman-counterservice.web.val.run';
 let number = 0;
-let interactionCount = 0;
 (function updateCounter() {
     fetch(`${url}/increment`, { method: 'POST' })
         .then(response => response.json())
@@ -34,39 +33,17 @@ function animateCounterChange(startNumber, endNumber) {
 }
 
 grow.addEventListener('click', () => {
-    interactionCount++;
-    var growth = Math.floor(Math.random() * interactionCount);
-    if (interactionCount > 1) {
-        number = number + Math.max(1, growth);
-        counter.innerText = formatCounter(number);
-        fetch(`${url}/set`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ number: number }),
+    number = number + 1;
+    counter.innerText = formatCounter(number);
+    fetch(`${url}/increment`, { method: 'POST' })
+        .then(response => response.json())
+        .then(data => {
+            animateCounterChange(number, data.number);
+            number = data.number;
         })
-            .then(response => response.json())
-            .then(data => {
-                animateCounterChange(number, data.number);
-                number = data.number;
-            })
-            .catch(error => {
-                alert('The counter is growing too quickly. Try again in a minute or so.');
-            });
-    } else {
-        number = number + 1;
-        counter.innerText = formatCounter(number);
-        fetch(`${url}/increment`, { method: 'POST' })
-            .then(response => response.json())
-            .then(data => {
-                animateCounterChange(number, data.number);
-                number = data.number;
-            })
-            .catch(error => {
-                alert('The counter is growing too quickly. Try again in a minute or so.');
-            });
-    }
+        .catch(error => {
+            alert('The counter is growing too quickly. Try again in a minute or so.');
+        });
 });
 
 kill.addEventListener('click', () => {
@@ -75,7 +52,6 @@ kill.addEventListener('click', () => {
     }
     counter.innerText = formatCounter(0);
     number = 0;
-    interactionCount = 0;
     fetch(`${url}/reset`, { method: 'POST' })
         .then(response => response.json())
         .then(data => {
