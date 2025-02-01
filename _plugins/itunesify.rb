66af0ceee32184c0b7099c12ebe88afaf8f06ebb
Josh Beckman <email@andjosh.com>
2025-02-01 10:33:28 -0600

    chg(dev): Obey the Layout/HeredocIndentation rubocop

--- _plugins/itunesify.rb
+++ _plugins/itunesify.rb
@@ -6,8 +6,8 @@ module Jekyll
     def itunesify(input)
       url = URI(input)
       url.host = "embed.music.apple.com"
-      block = <<-HTML
-<iframe allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write" frameborder="0" height="450" style="width:100%;max-width:660px;overflow:hidden;border-radius:10px;" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="#{url.to_s}"></iframe>
+      block = <<~HTML
+        <iframe allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write" frameborder="0" height="450" style="width:100%;max-width:660px;overflow:hidden;border-radius:10px;" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="#{url.to_s}"></iframe>
       HTML
       block
     end
