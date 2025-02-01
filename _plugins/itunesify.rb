2e364daf941aa2b4cf7702e555a1f58b77af6397
Josh Beckman <email@andjosh.com>
2025-02-01 10:42:48 -0600

    chg(dev): Obey the Lint rubocops

--- _plugins/itunesify.rb
+++ _plugins/itunesify.rb
@@ -8,7 +8,7 @@ module Jekyll
       url = URI(input)
       url.host = 'embed.music.apple.com'
       <<~HTML
-        <iframe allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write" frameborder="0" height="450" style="width:100%;max-width:660px;overflow:hidden;border-radius:10px;" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="#{url.to_s}"></iframe>
+        <iframe allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write" frameborder="0" height="450" style="width:100%;max-width:660px;overflow:hidden;border-radius:10px;" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="#{url}"></iframe>
       HTML
     end
   end
