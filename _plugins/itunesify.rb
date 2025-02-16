a1d783b4213cde96c2e8cf051e5e34881b5bdb0f
Josh Beckman <email@andjosh.com>
2025-02-16 11:25:03 -0600

    fix:usr: Fix itunesify embed height

--- _plugins/itunesify.rb
+++ _plugins/itunesify.rb
@@ -7,8 +7,12 @@ module Jekyll
     def itunesify(input)
       url = URI(input)
       url.host = 'embed.music.apple.com'
+      width = nil
+      # if the url has no query, then it's a full album
+      # and we need to set the height to 450px to avoid scrollbars
+      width = 'height="450"' if url.query.nil?
       <<~HTML
-        <iframe allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write" frameborder="0" height="450" style="width:100%;max-width:660px;overflow:hidden;border-radius:10px;" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="#{url}"></iframe>
+        <iframe allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write" frameborder="0" #{width} style="width:100%;max-width:660px;overflow:hidden;border-radius:10px;" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="#{url}"></iframe>
       HTML
     end
   end
